f5bf202b14489382e5352847de870754e16b022a
Patryk <patrykwidlazb@gmail.com>
2023-07-08 18:05:34 +0200

    Create appointment repo and make controller use it

--- AppointmentMicroservice/Application/Interfaces/IAppointmentRepository.cs
+++ AppointmentMicroservice/Application/Interfaces/IAppointmentRepository.cs
@@ -1,6 +1,15 @@
-﻿namespace AppointmentMicroservice.Application.Interfaces
+﻿using Domain.Entities;
+
+namespace AppointmentMicroservice.Application.Interfaces
 {
-    public class IAppointmentRepository
+    public interface IAppointmentRepository
     {
+        ICollection<Appointment> GetAppointments();
+        Appointment GetAppointment(int id);
+        bool AppointmentExists(int id);
+        bool CreateAppointment(Appointment appointment);
+        bool UpdateAppointment(Appointment appointment);
+        bool DeleteAppointment(int id);
+        bool Save();
     }
 }
---
+++ AppointmentMicroservice/Application/Repositories/AppointmentRepository.cs
@@ -0,0 +1,60 @@
+﻿using AppointmentMicroservice.Application.Interfaces;
+using Domain.Entities;
+using Persistence.Context;
+
+namespace AppointmentMicroservice.Application.Repositories
+{
+    public class AppointmentRepository : IAppointmentRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AppointmentRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public ICollection<Appointment> GetAppointments()
+        {
+            return _context.Appointments.ToList();
+        }
+
+        public Appointment GetAppointment(int id)
+        {
+            return _context.Appointments.FirstOrDefault(a => a.Id == id);
+        }
+
+        public bool AppointmentExists(int id)
+        {
+            return _context.Appointments.Any(a => a.Id == id);
+        }
+
+        public bool CreateAppointment(Appointment appointment)
+        {
+            _context.Appointments.Add(appointment);
+            return Save();
+        }
+
+        public bool UpdateAppointment(Appointment appointment)
+        {
+            _context.Appointments.Update(appointment);
+            return Save();
+        }
+
+        public bool DeleteAppointment(int id)
+        {
+            var appointment = _context.Appointments.FirstOrDefault(a => a.Id == id);
+            if (appointment != null)
+            {
+                _context.Appointments.Remove(appointment);
+                return Save();
+            }
+            return false;
+        }
+
+        public bool Save()
+        {
+            var saved = _context.SaveChanges();
+            return saved > 0 ? true : false;
+        }
+    }
+}
