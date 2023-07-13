a0b231fa991004b85c39f9446665223e3a2d0959
Patryk <patrykwidlazb@gmail.com>
2023-07-13 17:31:13 +0200

    Add new test scenarios

--- MedManager.Tests/Controller/AdminsControllerTests.cs
+++ MedManager.Tests/Controller/AdminsControllerTests.cs
@@ -1,15 +1,20 @@
 ﻿using AdminMicroservice.Application.Interfaces;
 using AdminMicroservice.Controllers;
 using Domain.Entities;
+using Domain.Validators;
 using FakeItEasy;
 using FluentAssertions;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Xunit;
 
 namespace MedManager.Tests.Controller
 {
     public class AdminsControllerTests
     {
         private readonly IAdminRepository _adminRepository;
+
         public AdminsControllerTests()
         {
             _adminRepository = A.Fake<IAdminRepository>();
@@ -24,7 +29,6 @@ namespace MedManager.Tests.Controller
 
             var result = controller.GetAdmins();
 
-            //Assert.NotNull(result);
             result.Should().NotBeNull();
             result.Should().BeOfType<ActionResult<IEnumerable<Admin>>>()
                 .Which.Result.Should().BeOfType<OkObjectResult>();
@@ -44,6 +48,43 @@ namespace MedManager.Tests.Controller
             var result = controller.CreateAdmin(adminCreate);
 
             result.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void AdminsController_UpdateAdmin_ReturnsBadRequest_WhenAdminIdDoesNotMatch()
+        {
+            var adminId = 1;
+            var admin = A.Fake<Admin>();
+            var controller = new AdminsController(_adminRepository);
+
+            var result = controller.UpdateAdmin(adminId + 1, admin);
+
+            result.Should().BeOfType<BadRequestResult>();
+        }
+
+        [Fact]
+        public void AdminsController_DeleteAdmin_ReturnsOK()
+        {
+            var adminId = 1;
+            A.CallTo(() => _adminRepository.AdminExists(adminId)).Returns(true);
+            A.CallTo(() => _adminRepository.DeleteAdmin(adminId)).Returns(true);
+            var controller = new AdminsController(_adminRepository);
+
+            var result = controller.DeleteAdmin(adminId);
+
+            result.Should().BeOfType<OkResult>();
+        }
+
+        [Fact]
+        public void AdminsController_DeleteAdmin_ReturnsNotFound_WhenAdminDoesNotExist()
+        {
+            var adminId = 1;
+            A.CallTo(() => _adminRepository.AdminExists(adminId)).Returns(false);
+            var controller = new AdminsController(_adminRepository);
+
+            var result = controller.DeleteAdmin(adminId);
+
+            result.Should().BeOfType<NotFoundResult>();
         }
     }
 }
